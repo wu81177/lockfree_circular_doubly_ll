ad63bdc88ce44bd016be71374d844796918481fa
wu81177 <wu81177@gmail.com>
2026-03-18 16:39:10 +0800

    Add prev pointer check

--- test.c
+++ test.c
@@ -21,21 +21,29 @@ void* thread_func(void* arg) {
 }
 
 void verify_list_integrity(struct list_head *h) {
-    printf("開始驗證串列線性化與結構完整性...\n");
+    printf("開始驗證串列線性化、結構完整性與 prev 指標...\n");
     
     struct list_head *curr = h->next; 
+    struct list_head *prev_node = h;
     val_t prev_val = LONG_MIN; 
     int internal_nodes = 0;
+    int prev_errors = 0;
 
     while (curr) {
-        node_t *curr_node = list_entry(curr, node_t, list);
+        uintptr_t curr_addr = (uintptr_t)curr & ~0x1L;
+        struct list_head *real_curr = (struct list_head *)curr_addr;
+        node_t *curr_node = list_entry(real_curr, node_t, list);
         val_t curr_val = curr_node->data;
         
         if (curr_val < prev_val) {
             printf("[錯誤] 發現排序異常: %ld 出現在 %ld 之後！\n", (long)curr_val, (long)prev_val);
             exit(1);
         }
-        
+
+        if (real_curr->prev != prev_node) {
+            prev_errors++;
+        }
+
         if (curr_val != (val_t)INT_MIN && curr_val != (val_t)INT_MAX) {
             internal_nodes++;
         }
@@ -43,8 +51,9 @@ void verify_list_integrity(struct list_head *h) {
         if (curr_val == (val_t)INT_MAX) break;
 
         prev_val = curr_val;
-        
-        uintptr_t next_raw = (uintptr_t)curr->next;
+        prev_node = real_curr;
+
+        uintptr_t next_raw = (uintptr_t)real_curr->next;
         curr = (struct list_head *)(next_raw & ~0x1L); 
 
         if (internal_nodes > NUM_THREADS * NUM_ITERATIONS + 100) {
@@ -52,7 +61,9 @@ void verify_list_integrity(struct list_head *h) {
             break;
         }
     }
-    printf("驗證成功！剩餘資料節點：%d\n", internal_nodes);
+    printf("驗證完成！\n");
+    printf("- 剩餘資料節點：%d\n", internal_nodes);
+    printf("- prev 指標錯誤總數：%d\n", prev_errors);
 }
 
 int main() {
